a81879a3a3b19702800ebe674397de8d389b8330
Dillon Grove <dillongrove@gmail.com>
2012-03-01 23:20:54 -0500

    Created deviceLoader.js and did function definitions for DeviceJS.java

--- android/RapidJSLibrary/src/com/eas/android/libraries/rapidjs/plugins/device/DeviceJS.java
+++ android/RapidJSLibrary/src/com/eas/android/libraries/rapidjs/plugins/device/DeviceJS.java
@@ -1,5 +1,49 @@
 package com.eas.android.libraries.rapidjs.plugins.device;
 
+import com.eas.android.libraries.rapidjs.pluginmanager.AcceleratedWebViewRequests;
+
 public class DeviceJS {
+	
+	private AcceleratedWebViewRequests requests;
+	
+	private String deviceName;
+	private String devicePhonegap;
+	private String devicePlatform;
+	private String deviceUUID;
+	private String deviceVersion;
+	
+	public DeviceJS(AcceleratedWebViewRequests requests){
+		this.requests = requests;
+		
+		
+
+	}
+	
+	private void setDeviceInformation(){
+		
+		
+	}
+	
+	public String getDeviceName(){
+		return deviceName;
+	}
+	
+	public String getDevicePhonegap(){
+		return devicePhonegap;
+	}
+	
+	public String getDevicePlatform(){
+		return devicePlatform;
+	}
+	
+	public String getDeviceUUID(){
+		return deviceUUID;
+	}
+	
+	public String getDeviceVersion(){
+		return deviceVersion;
+	}
+	
+	
 
 }
