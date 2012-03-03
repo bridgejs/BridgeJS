88a1a585e7a3eb7156bac8f7657b3a1095cdde47
Evan Shapiro <evan.a.shapiro@gmail.com>
2012-03-03 00:17:46 -0500

    started migration to new rapidjs client library

--- js/rapid.js
+++ js/rapid.js
@@ -1,11 +1,57 @@
 function Rapid () {
-    this.recommendNative = function () {
-        if (navigator.userAgent.match(/Android/i) && typeof ___androidExists !== "undefined") {
-            if(confirm("Do you want to run this website in Rapid?") ) {
-                location.replace("rapid://" + document.URL);
-            }
+
+	function browserIsAndroid(){
+		return navigator.userAgent.match(/Android/i);
+	}
+
+	function browserIsRapid(){
+		return (typeof ___androidExists !== "undefined");
+	}
+
+	function browserIsPhoneGap(){
+
+	}
+
+	function recommendUserSwitchesToRapid(){
+		if(confirm("Do you want to run this website in Rapid?")) {
+			switchThisURLToRapid();
+		}
+	}
+
+	function switchThisURLToRapid(){
+		changeURLTo("rapid://" + document.URL);
+	}
+
+	function changeURLTo(newURL){
+            location.replace(newUrl);
+	}
+
+//=========================================
+//client Android Switch to Rapid
+//=========================================
+
+	this.android = {};
+
+    android.recommendNative = function () {
+		if (browserIsAndroid() && !browserIsRapid()){
+			recommendUserSwitchesToRapid();
         }
     };
+
+    android.requireNative = function () {
+		if (browserIsAndroid() && !browserIsRapid()){
+			switchThisURLToRapid();
+        }
+    };
+
+//=========================================
+//Accelerometer wrapper
+//=========================================
+	this.accelerometer = {};
+	//accelerometer.getCurrentAcceleration = 
+	//accelerometer.watchAcceleration = 
+
 }
 
 var rapid = new Rapid();
+
