d04793e2a9c86003005dbefe0a5bac41754f935b
Evan Shapiro <evan.a.shapiro@gmail.com>
2012-03-06 00:48:29 -0500

    changed names of projects to match new name. Projects may need to be reimported into eclipse

---
+++ android/PhoneBridgeLibrary/src/org/bridgejs/android/phonebridge/library/plugins/accelerometer/AccelerometerPlugin.java
@@ -0,0 +1,36 @@
+package org.bridgejs.android.phonebridge.library.plugins.accelerometer;
+
+import org.bridgejs.android.phonebridge.library.pluginmanager.BridgeJSPlugin;
+import org.bridgejs.android.phonebridge.library.pluginmanager.PluginRequests;
+
+import android.graphics.Canvas;
+
+
+public class AccelerometerPlugin implements BridgeJSPlugin{
+
+	private String loadingJS;
+
+	public void init(PluginRequests requests) {
+
+		loadingJS = requests.getBridgeJSAsset("accelerometerLoader.js");
+
+		AccelerometerJS accelerometer = new AccelerometerJS(requests);
+		requests.addJavascriptInterface(accelerometer, "__androidAccelerometer");
+	}
+	
+	public String getPluginJS() {
+		return loadingJS;
+	}
+
+	public void onPageFinishedLoading() {
+	}
+
+	public void onDraw(Canvas canvas, int left, int top, float scale) {
+
+	}
+
+	public void onPageStartedLoading() {
+
+	}
+
+}
