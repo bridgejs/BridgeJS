2c6c4754118a230312ed5db51a591ed8fe530a5a
Brandon Kase <bkase@cmu.edu>
2012-03-08 00:37:36 -0500

    Made the node server.js file a simple static server

--- http/server.js
+++ http/server.js
@@ -1,28 +1,40 @@
-var express = require('express');
-var app = express.createServer();
+/* Simple static webserver from https://gist.github.com/1926868 */
 
-app.get('/', function(req, res){
-	res.sendfile(__dirname + '/index.html');
-});
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    mime = require("mime")
+    port = process.argv[2] || 80;
 
-app.get('/server.js', function(req, res){
-	res.sendfile(__dirname + '/server.js');
-});
+http.createServer(function(request, response) {
 
-app.get('/bridge.js', function(req, res){
-	res.sendfile(__dirname + '/js/bridge.js');
-});
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+  
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
 
-app.get('/demos/gameNative.html', function(req, res){
-	res.sendfile(__dirname + '/demos/gameNative.html');
-});
+	if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-app.get('/demos/gamePhoneGap.html', function(req, res){
-	res.sendfile(__dirname + '/demos/gamePhoneGap.html');
-});
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
 
-app.get('/launcher', function(req, res){
-	res.sendfile(__dirname + '/launcher/launcher.html');
-});
+      response.writeHead(200, {"Content-Type": mime.lookup(filename)});
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
 
-app.listen(80);
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
